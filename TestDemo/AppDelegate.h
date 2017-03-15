9112468bb2c0efee42a8fa0c6e5b93a8dedbea85
admin <admin@wangyafeideMacBook-Pro.local>
2017-03-15 16:36:34 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by admin on 2017/3/15.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by admin on 2017/3/15.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
